cab0ccb37f3ce63cd1d8edb073763474f6e7b51d
Darshan Ponikar <drsofficial3k@gmail.com>
2024-09-05 17:08:21 +0530

    working with api integreation

--- packages/app-store/huddle-01/lib/VideoApiAdapter.ts
+++ packages/app-store/huddle-01/lib/VideoApiAdapter.ts
@@ -1,6 +1,29 @@
+import { getHuddle01APIKey, getHuddle01Credential } from "huddle-01/utils/storage";
+
 import logger from "@calcom/lib/logger";
 import type { CalendarEvent } from "@calcom/types/Calendar";
 import type { VideoApiAdapter } from "@calcom/types/VideoApiAdapter";
+
+const fetchHuddleAPI = async (userId: number) => {
+  const API_END_POINT = "https://platform-api-darshan.huddle01.workers.dev/api/v2/calendar";
+
+  const { identityToken } = await getHuddle01Credential(userId);
+  const { apiKey } = await getHuddle01APIKey();
+
+  const headers = {
+    "x-api-key": apiKey,
+    "x-identity-token": identityToken,
+  };
+  return (endpoint: "subdomains" | "createMeeting", option?: RequestInit) => {
+    return fetch(`${API_END_POINT}/${endpoint}`, {
+      ...option,
+      headers: {
+        ...option?.headers,
+        ...headers,
+      },
+    });
+  };
+};
 
 const log = logger.getSubLogger({ prefix: ["app-store/huddle-01/lib/VideoApiAdapter"] });
 
